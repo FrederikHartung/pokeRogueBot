3c0f921b4cd1afbe4e69e1936b3da0fddd4ab4bc
FrederikHartung <90627809+FrederikHartung@users.noreply.github.com>
2024-05-26 15:36:36 +0200

    Initial commit

---
+++ src/test/java/com/sfh/pokeRogueBot/PokeRogueBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfh.pokeRogueBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeRogueBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
