5fad1576bfa1c56edc427b761b7659c71e477e5f
FrederikHartung <90627809+FrederikHartung@users.noreply.github.com>
2024-06-05 11:05:09 +0200

    Update GameSettingConstants.java

--- src/main/java/com/sfh/pokeRogueBot/config/GameSettingConstants.java
+++ src/main/java/com/sfh/pokeRogueBot/config/GameSettingConstants.java
@@ -17,7 +17,8 @@ public class GameSettingConstants {
             "MUTE", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"}, 10);
     private static final GameSettingProperty seVolume = new GameSettingProperty(new String[]{
             "MUTE", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"}, 10);
-    private static final GameSettingProperty langruage = new GameSettingProperty(new String[0], 0); //not used because it restarts the game
+    private static final GameSettingProperty langruage = new GameSettingProperty(new String[]{
+            "english", "not supported"} , 0); //this may reload the game
     private static final GameSettingProperty damageNumbers = new GameSettingProperty(new String[]{
             "OFF", "Simple", "Fancy"}, 0);
     private static final GameSettingProperty uiTheme = new GameSettingProperty(new String[0], 0); //not used because it restarts the game
