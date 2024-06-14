26fb5278b0e031511bebac425c068eae816cbbd6
FrederikHartung <90627809+FrederikHartung@users.noreply.github.com>
2024-06-14 08:28:06 +0200

    added missing phases

---
+++ src/main/java/com/sfh/pokeRogueBot/phase/impl/MoneyRewardPhase.java
@@ -0,0 +1,30 @@
+package com.sfh.pokeRogueBot.phase.impl;
+
+import com.sfh.pokeRogueBot.model.browser.enums.GameMode;
+import com.sfh.pokeRogueBot.model.exception.NotSupportedException;
+import com.sfh.pokeRogueBot.phase.AbstractPhase;
+import com.sfh.pokeRogueBot.phase.Phase;
+import com.sfh.pokeRogueBot.phase.actions.PhaseAction;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MoneyRewardPhase extends AbstractPhase implements Phase {
+
+    public static final String NAME = "MoneyRewardPhase";
+
+    @Override
+    public String getPhaseName() {
+        return NAME;
+    }
+
+    @Override
+    public PhaseAction[] getActionsForGameMode(GameMode gameMode) throws NotSupportedException {
+        if(gameMode == GameMode.MESSAGE)
+            return new PhaseAction[]{
+                    this.pressSpace
+            };
+
+        throw new NotSupportedException("GameMode not supported for MoneyRewardPhase: " + gameMode);
+    }
+
+}
