86474e2b9eaa101ca81069c5b2138c877feb18fe
FrederikHartung <90627809+FrederikHartung@users.noreply.github.com>
2024-06-20 06:19:24 +0200

    Create ChosePokeBallModifierNeuron.java

---
+++ src/main/java/com/sfh/pokeRogueBot/service/neurons/ChosePokeBallModifierNeuron.java
@@ -0,0 +1,59 @@
+package com.sfh.pokeRogueBot.service.neurons;
+
+import com.sfh.pokeRogueBot.model.enums.PokeBallType;
+import com.sfh.pokeRogueBot.model.modifier.ModifierShop;
+import com.sfh.pokeRogueBot.model.modifier.ModifierShopItem;
+import com.sfh.pokeRogueBot.model.modifier.impl.AddPokeballModifierItem;
+import com.sfh.pokeRogueBot.model.run.Wave;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+public class ChosePokeBallModifierNeuron {
+
+    public boolean priorityItemExists(ModifierShop shop, Wave wave){
+        //if a special pokeball is available, pick it
+        ModifierShopItem pokeBallModifier = shop.getFreeItems().stream()
+                .filter(item -> item.getItem().getTypeName().equals(AddPokeballModifierItem.TARGET))
+                .findFirst()
+                .orElse(null);
+        if(null != pokeBallModifier){
+            int rogueBalls = wave.getPokeballCount()[PokeBallType.ROGUE_BALL.ordinal()];
+            int ultraBalls = wave.getPokeballCount()[PokeBallType.ULTRA_BALL.ordinal()];
+            int greatBalls = wave.getPokeballCount()[PokeBallType.GREAT_BALL.ordinal()];
+            int pokeBalls = wave.getPokeballCount()[PokeBallType.POKEBALL.ordinal()];
+
+            switch (((AddPokeballModifierItem)pokeBallModifier.getItem()).getPokeballType()){
+                case MASTER_BALL, LUXURY_BALL: //always pick these
+                    return true;
+                case ROGUE_BALL:
+                    if(rogueBalls <= 5){
+                        return true;
+                    }
+                    break;
+                case ULTRA_BALL:
+
+                    if((rogueBalls + ultraBalls) <= 5){
+                        return true;
+                    }
+                    break;
+                case GREAT_BALL:
+                    if((rogueBalls + ultraBalls + greatBalls) <= 5){
+                        return true;
+                    }
+                    break;
+                case POKEBALL:
+                    if((rogueBalls + ultraBalls + greatBalls + pokeBalls) <= 5){
+                        return true;
+                    }
+                    break;
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
