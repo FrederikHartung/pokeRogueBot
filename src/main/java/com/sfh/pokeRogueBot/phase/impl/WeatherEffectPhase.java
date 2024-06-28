1ada7f87ab0a69d7e6a686b7ade5359ac5cbebab
FrederikHartung <90627809+FrederikHartung@users.noreply.github.com>
2024-06-28 11:21:52 +0200

    Update WeatherEffectPhase.java

--- src/main/java/com/sfh/pokeRogueBot/phase/impl/WeatherEffectPhase.java
+++ src/main/java/com/sfh/pokeRogueBot/phase/impl/WeatherEffectPhase.java
@@ -1,5 +1,30 @@
 package com.sfh.pokeRogueBot.phase.impl;
 
-public class WeatherEffectPhase {
-    //COMMAND
+import com.sfh.pokeRogueBot.model.enums.GameMode;
+import com.sfh.pokeRogueBot.model.exception.NotSupportedException;
+import com.sfh.pokeRogueBot.phase.AbstractPhase;
+import com.sfh.pokeRogueBot.phase.Phase;
+import com.sfh.pokeRogueBot.phase.actions.PhaseAction;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WeatherEffectPhase extends AbstractPhase implements Phase {
+
+    public static final String NAME = "WeatherEffectPhase";
+
+    @Override
+    public String getPhaseName() {
+        return NAME;
+    }
+
+    @Override
+    public PhaseAction[] getActionsForGameMode(GameMode gameMode) throws NotSupportedException {
+        if(gameMode == GameMode.COMMAND){
+            return new PhaseAction[]{
+                    waitAction
+            };
+        }
+
+        throw new NotSupportedException("WeatherEffectPhase does not support GameMode: " + gameMode);
+    }
 }
