45af780c3fbdbea5ac6ba64433d8585dbda52d9f
FrederikHartung <90627809+FrederikHartung@users.noreply.github.com>
2024-07-02 14:19:22 +0200

    Update ChooseModifierItem.java

--- src/main/java/com/sfh/pokeRogueBot/model/modifier/ChooseModifierItem.java
+++ src/main/java/com/sfh/pokeRogueBot/model/modifier/ChooseModifierItem.java
@@ -1,8 +1,15 @@
 package com.sfh.pokeRogueBot.model.modifier;
 
+import com.google.common.reflect.TypeToken;
 import com.sfh.pokeRogueBot.model.enums.ModifierTier;
 
+import java.lang.reflect.Type;
+import java.util.List;
+
 public interface ChooseModifierItem {
+
+    Type LIST_TYPE = new TypeToken<List<ChooseModifierItem>>() {
+    }.getType();
 
     String getId();
 
