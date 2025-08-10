02d90bcd3d4b6283e140c6d559a9c41eaee14b9e
FrederikHartung <90627809+FrederikHartung@users.noreply.github.com>
2025-08-10 18:58:45 +0200

    updated contructor

--- src/main/java/com/sfh/pokeRogueBot/model/exception/UnsupportedPhaseException.java
+++ src/main/java/com/sfh/pokeRogueBot/model/exception/UnsupportedPhaseException.java
@@ -4,11 +4,11 @@ import com.sfh.pokeRogueBot.model.enums.UiMode;
 
 public class UnsupportedPhaseException extends RuntimeException {
 
-    public UnsupportedPhaseException(String message) {
-        super(message);
-    }
-
     public UnsupportedPhaseException(String phase, UiMode gameMode) {
         super("Phase " + phase + " with GameMode " + gameMode + " is not supported yet");
+    }
+
+    public UnsupportedPhaseException(String phase) {
+        super("Phase " + phase + " is not supported yet");
     }
 }
